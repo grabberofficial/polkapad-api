21649239898d27ae21ac1d0fc25caa8802de0186
ilyaviache <ilya.viache@gmail.com>
2022-05-08 14:42:24 +0700

    Revert "Revert "Remove legacy, fix tests""
    This reverts commit 4b7e1b5d575b13e03ed9e8bb4b0fe2c5448ebe8d.

--- src/_legacy/users/dto/create-user-otp.dto.ts
+++
@@ -1,8 +0,0 @@
-import { ApiProperty } from '@nestjs/swagger';
-import { IsEmail, IsNotEmpty } from 'class-validator';
-
-export class CreateUserOtpDto {
-  @ApiProperty()
-  @IsEmail({}, { message: 'Email is invalid' })
-  email: string;
-}

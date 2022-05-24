51b7465caa721e370969c1fa03207f44e5d8c3ad
vnkitaev <vnkitayev@gmail.com>
2022-05-24 13:46:48 +0400

    fix: Fix kyc errors

--- src/controllers/kyc.controller.ts
+++ src/controllers/kyc.controller.ts
@@ -4,6 +4,7 @@ import { ApiBearerAuth, ApiTags, ApiOkResponse } from '@nestjs/swagger';
 import { KycService } from 'services';
 import { AuthGuard } from 'guards';
 import { UserContext } from 'decorators';
+import { InternalServerErrorException } from 'exceptions';
 import { UserContextModel } from 'models';
 import { KycCallbackDto } from 'dto/kyc';
 
@@ -24,7 +25,13 @@ export class KycController {
   @UseGuards(AuthGuard)
   @ApiBearerAuth()
   @ApiOkResponse({ type: String })
-  getVerificationUrl(@UserContext() userContext: UserContextModel) {
-    return this.kycService.getVerificationUrl(userContext.id);
+  async getVerificationUrl(@UserContext() userContext: UserContextModel) {
+    try {
+      return await this.kycService.getVerificationUrl(userContext.id);
+    } catch (e) {
+      console.error(e);
+
+      throw new InternalServerErrorException('Failed to get verification url');
+    }
   }
 }

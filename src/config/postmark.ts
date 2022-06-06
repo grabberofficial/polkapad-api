89722370b881badd2061d8784521718b33e7c211
i-kitaev <vnkitayev@gmail.com>
2022-06-06 10:29:38 +0300

    Dev (#19)
    * feat: Kyc save callbacks
    feat: Kyc exception if already accepted
    fix: Magic links
    
    * fix: Postmark envs

--- src/config/postmark.ts
+++ src/config/postmark.ts
@@ -1,3 +1,4 @@
-const { POSTMARK_TOKEN } = process.env;
+const { POSTMARK_TOKEN, POSTMARK_FROM } = process.env;
 
 export const token = POSTMARK_TOKEN;
+export const from = POSTMARK_FROM || 'hello@polkapad.network';

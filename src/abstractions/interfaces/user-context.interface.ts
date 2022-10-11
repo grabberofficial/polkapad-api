4e779653a42611f7e6b73ca47b31cd9802dd340f
i-kitaev <vnkitayev@gmail.com>
2022-10-11 17:40:09 +0400

    feat: Sales & auth updates (#47)

--- src/abstractions/interfaces/user-context.interface.ts
+++ src/abstractions/interfaces/user-context.interface.ts
@@ -2,6 +2,7 @@ import { KycStatusTypes, UserRoleTypes } from '@prisma/client';
 
 export interface IUserContext {
   id: string;
+  email?: string;
   name?: string;
   kycStatus?: KycStatusTypes;
   role?: UserRoleTypes;
